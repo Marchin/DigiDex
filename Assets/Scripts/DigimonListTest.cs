5f22bb0e2de87b5cd3069d77116c9bd24c8f5ea8
Martín Monterrosa <mmonterrosa94@gmail.com>
2021-09-10 00:09:57 -0300

    Refactor ButtonScrollList into it's own class

--- Assets/Scripts/DigimonListTest.cs
+++ Assets/Scripts/DigimonListTest.cs
@@ -10,21 +10,12 @@ using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 
 public class DigimonListTest : MonoBehaviour {
-    [SerializeField] private float _scrollAnimationOffset = 550f;
-    [SerializeField] private float _scrollAnimationScale = 0.2f;
-    [SerializeField] private float _scrollCenteringSpeedMul = 2f;
     [SerializeField] private Image _digimonImage = default;
     [SerializeField] private TextMeshProUGUI _digimonName = default;
     [SerializeField] private TextMeshProUGUI _digimonProfile = default;
     [SerializeField] private TMP_InputField _searchInput = default;
-    [SerializeField] private CustomScrollRect _scrollRect = default;
-    [SerializeField] private Color _unselectedColor = default;
-    [SerializeField] private Color _selectedColor = default;
     [SerializeField] private CustomScrollRect _infoScroll = default;
     [SerializeField] private InformationRowList _info = default;
-    [SerializeField] private VerticalLayoutGroup _layoutGroup = default;
-    [SerializeField] private RectTransform _buttonTemplate = default;
-    [SerializeField] private int _maxButtons = default;
     [SerializeField] private Button _profileButton = default;
     [SerializeField] private Toggle _dataToggle = default;
     [SerializeField] private Toggle _profileToggle = default;
@@ -32,29 +23,11 @@ public class DigimonListTest : MonoBehaviour {
     [SerializeField] private RectTransform _profileContent = default;
     [SerializeField] private Animator _profileAnimator = default;
     [SerializeField] private DigimonDatabase _digimonDB = default;
-    private Button[] _digimonButtons;
-    private TextMeshProUGUI[] _digimonButtonsTexts;
-    private float _buttonNormalizeLenght;
-    private float _buttonReuseScrollPoint = 0.3f;
-    private int _currDigimonScrollIndex;
-    private int _selectedDigimonIndex;
+    [SerializeField] private ButtonScrollList _animatedScroll = default;
     private CancellationTokenSource _digimonDataCTS;
     private List<AsyncOperationHandle> _digimonDataHandles = new List<AsyncOperationHandle>();
-    private bool _profileOpen = false;
-    private bool _fixingListPosition = false;
     private List<DigimonReference> _currDigimonList;
-    public Button CurrenButton => _digimonButtons[_currButtonIndex];
-    public bool IsEmpty => _currDigimonList.Count == 0;
-    private int _currButtonIndex;
-    public int CurrButtonIndex {
-        get => _currButtonIndex;
-        set {
-            if (_currButtonIndex != value) {
-                _currButtonIndex = value;
-                RefreshButtons();
-            }
-        }
-    }
+    private bool _profileOpen = false;
     
     private DigimonReference _selectedDigimon;
     public DigimonReference SelectedDigimon {
@@ -79,8 +52,7 @@ public class DigimonListTest : MonoBehaviour {
 
             _digimonImage.gameObject.SetActive(false);
             if (_selectedDigimon != null) {
-                _selectedDigimonIndex = _currDigimonList.IndexOf(SelectedDigimon);
-                RefreshButtons();
+                _animatedScroll.RefreshButtons();
                 var dataHandle = Addressables.LoadAssetAsync<Digimon>(value.Data);
                 _digimonDataHandles.Add(dataHandle);
                 dataHandle.WithCancellation(_digimonDataCTS.Token).ContinueWith(digimon => {
@@ -108,14 +80,13 @@ public class DigimonListTest : MonoBehaviour {
                     }
                 }).Forget();
             } else {
-                _selectedDigimonIndex = 0;
                 _digimonName.text = "";
                 _digimonProfile.text = "";
             }
         }
     }
 
-    private async void Start() {
+    private void Start() {
         if (_digimonDB == null) {
 #if UNITY_EDITOR
             UnityEditor.EditorApplication.ExitPlaymode();
@@ -126,154 +97,45 @@ public class DigimonListTest : MonoBehaviour {
         }
 
         _currDigimonList = _digimonDB.Digimons;
+        _animatedScroll.Initialize(
+            nameList: _currDigimonList.Select(d => d.Name).ToList(),
+            onSelected: (index) => {
+                if (index >= 0 && index <= _currDigimonList.Count) {
+                    SelectedDigimon = _currDigimonList[index];
+                    _profileButton.gameObject.SetActive(true);
+                } else {
+                    SelectedDigimon = null;
+                    _profileButton.gameObject.SetActive(false);
+                }
+            }
+        );
+
+        _animatedScroll.OnBeginDrag += () => _profileButton.gameObject.SetActive(false);
 
         _searchInput.onValueChanged.AddListener(OnInputChanged);
-
-        int buttonCount = Mathf.Min(_digimonDB.Digimons.Count, _maxButtons);
-        for (int i = 0; i < buttonCount; i++) {
-            Instantiate(_buttonTemplate, _scrollRect.content);
-        }
-
-        _scrollRect.OnBeginDragEvent += () => {
-            _profileButton.gameObject.SetActive(false);
-            _fixingListPosition = false;
-        };
-        _scrollRect.OnEndDragEvent += () => _fixingListPosition = !IsEmpty;
 
         _dataToggle.onValueChanged.AddListener(isOn => _dataContent.gameObject.SetActive(isOn));
         _profileToggle.onValueChanged.AddListener(isOn => _profileContent.gameObject.SetActive(isOn));
 
-        // Wait for button population
-        await UniTask.DelayFrame(1);
-        
-        // Add padding so that the top and botton buttonns can be centered when scrolling
-        _layoutGroup.padding.top = _layoutGroup.padding.bottom =
-            Mathf.RoundToInt(0.5f * (_scrollRect.viewport.rect.height - _buttonTemplate.rect.height));
-
-        _buttonTemplate.gameObject.SetActive(false);
-        _digimonButtons = _scrollRect.content.GetComponentsInChildren<Button>();
-        _digimonButtonsTexts = _scrollRect.content.GetComponentsInChildren<TextMeshProUGUI>();
-
-        PopulateButtons();
-
         _profileButton.onClick.AddListener(() => {
             if (_profileOpen) {
-                _scrollRect.enabled = true;
+                _animatedScroll.ScrollEnabled = true;
                 _profileAnimator.SetTrigger("Fade Out");
             } else {
-                _scrollRect.enabled = false;
+                _animatedScroll.ScrollEnabled = false;
                 _profileAnimator.SetTrigger("Fade In");
             }
             _profileOpen = !_profileOpen;
         });
-
-        SelectedDigimon = _digimonDB.Digimons[0];
-
-        _scrollRect.onValueChanged.AddListener(OnScroll);
-
-        _scrollRect.normalizedPosition = Vector2.up;
-
-        // If we don't wait these frames the buttons don't get properly animated
-        await UniTask.DelayFrame(2);
-        AnimateButtons();
-    }
-
-    private void Update() {
-        if (_fixingListPosition) {
-            if (Mathf.Abs(_scrollRect.velocity.y) < 20f) {
-                Vector2 viewportCenter = _scrollRect.viewport.transform.position;
-                Vector2 selectedButtonPos = CurrenButton.transform
-                    .TransformPoint((CurrenButton.transform as RectTransform).rect.center);
-                if (!_scrollRect.BeingDragged) {
-                    _scrollRect.velocity = _scrollCenteringSpeedMul * Vector2.up * (viewportCenter.y - selectedButtonPos.y);
-                }
-
-                if (Mathf.Abs(viewportCenter.y - selectedButtonPos.y) < 30f) {
-                    SelectedDigimon = _currDigimonList[_currButtonIndex + _currDigimonScrollIndex];
-                    _profileButton.gameObject.SetActive(true);
-                    if (Mathf.Abs(viewportCenter.y - selectedButtonPos.y) < 2f) {
-                        _fixingListPosition = false;
-                    }
-                }
-            }
-        }
     }
 
     private void OnInputChanged(string query) {
-        _fixingListPosition = false;
         _currDigimonList = _digimonDB.Digimons
             .Where(digimon => digimon.Name.ToLower().Contains(query.ToLower()))
             .OrderByDescending(d => d.Name.StartsWith(query, true, CultureInfo.InvariantCulture))
             .ToList();
 
-        if (_currDigimonList.Count > 0) {
-            SelectedDigimon = _currDigimonList[0];
-        } else {
-            SelectedDigimon = null;
-        }
-
-        _scrollRect.normalizedPosition = Vector2.up;
-        _currDigimonScrollIndex = 0;
-        PopulateButtons();
+        _animatedScroll.UpdateList(_currDigimonList.Select(d => d.Name).ToList());
     }
 
-    private void OnScroll(Vector2 newPos) {
-        int newScrollIndex = _currDigimonScrollIndex;
-        if (_currDigimonScrollIndex < (_currDigimonList.Count - _digimonButtons.Length) && _scrollRect.velocity.y > 0f && newPos.y < ((_buttonReuseScrollPoint))) {
-            newScrollIndex++;
-            CurrButtonIndex--;
-            _scrollRect.CustomSetVerticalNormalizedPosition(_scrollRect.normalizedPosition.y + _buttonNormalizeLenght);
-        } else if (_currDigimonScrollIndex > 0 && _scrollRect.velocity.y < 0f && newPos.y > (1f - (_buttonReuseScrollPoint))) {
-            newScrollIndex--;
-            CurrButtonIndex++;
-            _scrollRect.CustomSetVerticalNormalizedPosition(_scrollRect.normalizedPosition.y - _buttonNormalizeLenght);
-        }
-        if (newScrollIndex != _currDigimonScrollIndex) {
-            _currDigimonScrollIndex = newScrollIndex;
-            PopulateButtons();
-        }
-
-        AnimateButtons();
-    }
-
-    private void AnimateButtons() {
-        float minT = float.MaxValue;
-        Vector2 viewportCenter = _scrollRect.viewport.transform.position;
-        Vector2 selectedButtonPos = Vector2.zero;
-        for (int iButton = 0; iButton < _digimonButtons.Length; iButton++) {
-            Vector2 button = _digimonButtons[iButton].transform.TransformPoint((_digimonButtons[iButton].transform as RectTransform).rect.center);
-            float t = Mathf.Abs(viewportCenter.y - button.y) / _scrollRect.viewport.rect.height;
-            button.x = viewportCenter.x - Mathf.Lerp(0, _scrollAnimationOffset, t*t);
-            _digimonButtons[iButton].transform.localScale = Vector2.one * Mathf.Lerp(1f, _scrollAnimationScale, t*t);
-            _digimonButtons[iButton].transform.position = button;
-            if (minT > t) {
-                selectedButtonPos = button;
-                minT = t;
-                CurrButtonIndex = iButton;
-            }
-        }
-    }
-
-    private void PopulateButtons() {
-        _currDigimonScrollIndex = Mathf.Min(_currDigimonScrollIndex, Mathf.Max(_currDigimonList.Count - _digimonButtons.Length, 0));
-        for (int iDigimon = _currDigimonScrollIndex, iButton = 0; iButton < _digimonButtons.Length; iDigimon++, iButton++) {
-            if (iButton < _currDigimonList.Count) {
-                _digimonButtonsTexts[iButton].text = _currDigimonList[iDigimon].Name;
-                _digimonButtons[iButton].gameObject.SetActive(true);
-            } else {
-                _digimonButtons[iButton].gameObject.SetActive(false);
-            }
-        }
-
-        float scrollableLength = _scrollRect.content.rect.height - _scrollRect.viewport.rect.height;
-        _buttonNormalizeLenght = ((_digimonButtons[0].transform as RectTransform).rect.height + _layoutGroup.spacing) /  scrollableLength;
-        RefreshButtons();
-    }
-
-    private void RefreshButtons() {
-        for (int iButton = 0; iButton < _digimonButtons.Length; iButton++) {
-            _digimonButtons[iButton].image.color = iButton == CurrButtonIndex ?
-                _selectedColor : _unselectedColor;
-        }
-    }
 }
---
+++ Assets/Scripts/UI/ButtonScrollList.cs
@@ -0,0 +1,196 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Cysharp.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+
+public class ButtonScrollList : MonoBehaviour {
+    [SerializeField] private float _scrollAnimationOffset = 550f;
+    [SerializeField] private float _scrollAnimationScale = 0.2f;
+    [SerializeField] private float _scrollCenteringSpeedMul = 2f;
+    [SerializeField] private CustomScrollRect _scrollRect = default;
+    [SerializeField] private Color _unselectedColor = default;
+    [SerializeField] private Color _selectedColor = default;
+    [SerializeField] private VerticalLayoutGroup _layoutGroup = default;
+    [SerializeField] private RectTransform _buttonTemplate = default;
+    [SerializeField] private int _maxButtons = default;
+    private Button[] _buttons;
+    private TextMeshProUGUI[] _buttonsTexts;
+    private List<string> _namesList = new List<string>();
+    private float _buttonNormalizedLenght;
+    private float _buttonReuseScrollPoint = 0.3f;
+    private int _currElementScrollIndex;
+    public Button CurrenButton => _buttons[_currButtonIndex];
+    private bool _fixingListPosition = false;
+    private Action<int> OnSelected;
+    public event Action OnBeginDrag;
+    public event Action OnEndDrag;
+    public bool IsEmpty => _namesList.Count == 0;
+    public bool ScrollEnabled {
+        get => _scrollRect.enabled;
+        set => _scrollRect.enabled = value;
+    }
+    private int _currButtonIndex;
+    public int CurrButtonIndex {
+        get => _currButtonIndex;
+        set {
+            if (_currButtonIndex != value) {
+                _currButtonIndex = value;
+                RefreshButtons();
+            }
+        }
+    }
+
+    public async void Initialize(List<string> nameList, Action<int> onSelected) {
+        if (nameList == null) {
+            Debug.LogError("List of names is null");
+            return;
+        }
+
+        OnSelected = onSelected;
+
+        _namesList = nameList;
+
+        int buttonCount = Mathf.Min(nameList.Count, _maxButtons);
+        for (int i = 0; i < buttonCount; i++) {
+            Instantiate(_buttonTemplate, _scrollRect.content);
+        }
+
+        _scrollRect.OnBeginDragEvent += () => {
+            _fixingListPosition = false;
+            OnBeginDrag?.Invoke();
+        };
+        _scrollRect.OnEndDragEvent += () => {
+            _fixingListPosition = !IsEmpty;
+            OnEndDrag?.Invoke();
+        };
+
+        // Wait for button population
+        await UniTask.DelayFrame(1);
+        
+        // Add padding so that the top and botton buttonns can be centered when scrolling
+        _layoutGroup.padding.top = _layoutGroup.padding.bottom =
+            Mathf.RoundToInt(0.5f * (_scrollRect.viewport.rect.height - _buttonTemplate.rect.height));
+
+        _buttonTemplate.gameObject.SetActive(false);
+        _buttons = _scrollRect.content.GetComponentsInChildren<Button>();
+        _buttonsTexts = _scrollRect.content.GetComponentsInChildren<TextMeshProUGUI>();
+
+        PopulateButtons();
+
+        OnSelected?.Invoke(0);
+
+        _scrollRect.onValueChanged.AddListener(OnScroll);
+
+        _scrollRect.normalizedPosition = Vector2.up;
+
+        // If we don't wait these frames the buttons don't get properly animated
+        await UniTask.DelayFrame(2);
+        AnimateButtons();
+    }
+
+    public void UpdateList(List<string> nameList) {
+        _fixingListPosition = false;
+        if (nameList == null) {
+            Debug.LogError("List of names is null");
+            return;
+        }
+        _namesList = nameList;
+
+        if (_namesList.Count > 0) {
+            OnSelected?.Invoke(0);
+        } else {
+            OnSelected?.Invoke(-1);
+        }
+
+        ResetScroll();
+    }
+
+    private void Update() {
+        if (_fixingListPosition) {
+            if (Mathf.Abs(_scrollRect.velocity.y) < 20f) {
+                Vector2 viewportCenter = _scrollRect.viewport.transform.position;
+                Vector2 selectedButtonPos = CurrenButton.transform
+                    .TransformPoint((CurrenButton.transform as RectTransform).rect.center);
+                if (!_scrollRect.BeingDragged) {
+                    _scrollRect.velocity = _scrollCenteringSpeedMul * Vector2.up * (viewportCenter.y - selectedButtonPos.y);
+                }
+
+                if (Mathf.Abs(viewportCenter.y - selectedButtonPos.y) < 30f) {
+                    OnSelected?.Invoke(_currButtonIndex + _currElementScrollIndex);
+                    // _profileButton.gameObject.SetActive(true);
+                    if (Mathf.Abs(viewportCenter.y - selectedButtonPos.y) < 2f) {
+                        _fixingListPosition = false;
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnScroll(Vector2 newPos) {
+        int newScrollIndex = _currElementScrollIndex;
+        if (_currElementScrollIndex < (_namesList.Count - _buttons.Length) && _scrollRect.velocity.y > 0f && newPos.y < ((_buttonReuseScrollPoint))) {
+            newScrollIndex++;
+            CurrButtonIndex--;
+            _scrollRect.CustomSetVerticalNormalizedPosition(_scrollRect.normalizedPosition.y + _buttonNormalizedLenght);
+        } else if (_currElementScrollIndex > 0 && _scrollRect.velocity.y < 0f && newPos.y > (1f - (_buttonReuseScrollPoint))) {
+            newScrollIndex--;
+            CurrButtonIndex++;
+            _scrollRect.CustomSetVerticalNormalizedPosition(_scrollRect.normalizedPosition.y - _buttonNormalizedLenght);
+        }
+        if (newScrollIndex != _currElementScrollIndex) {
+            _currElementScrollIndex = newScrollIndex;
+            PopulateButtons();
+        }
+
+        AnimateButtons();
+    }
+
+    private void AnimateButtons() {
+        float minT = float.MaxValue;
+        Vector2 viewportCenter = _scrollRect.viewport.transform.position;
+        Vector2 selectedButtonPos = Vector2.zero;
+        for (int iButton = 0; iButton < _buttons.Length; iButton++) {
+            Vector2 button = _buttons[iButton].transform.TransformPoint((_buttons[iButton].transform as RectTransform).rect.center);
+            float t = Mathf.Abs(viewportCenter.y - button.y) / _scrollRect.viewport.rect.height;
+            button.x = viewportCenter.x - Mathf.Lerp(0, _scrollAnimationOffset, t*t);
+            _buttons[iButton].transform.localScale = Vector2.one * Mathf.Lerp(1f, _scrollAnimationScale, t*t);
+            _buttons[iButton].transform.position = button;
+            if (minT > t) {
+                selectedButtonPos = button;
+                minT = t;
+                CurrButtonIndex = iButton;
+            }
+        }
+    }
+
+    private void PopulateButtons() {
+        _currElementScrollIndex = Mathf.Min(_currElementScrollIndex, Mathf.Max(_namesList.Count - _buttons.Length, 0));
+        for (int iDigimon = _currElementScrollIndex, iButton = 0; iButton < _buttons.Length; iDigimon++, iButton++) {
+            if (iButton < _namesList.Count) {
+                _buttonsTexts[iButton].text = _namesList[iDigimon];
+                _buttons[iButton].gameObject.SetActive(true);
+            } else {
+                _buttons[iButton].gameObject.SetActive(false);
+            }
+        }
+
+        float scrollableLength = _scrollRect.content.rect.height - _scrollRect.viewport.rect.height;
+        _buttonNormalizedLenght = ((_buttons[0].transform as RectTransform).rect.height + _layoutGroup.spacing) /  scrollableLength;
+        RefreshButtons();
+    }
+
+    public void RefreshButtons() {
+        for (int iButton = 0; iButton < _buttons.Length; iButton++) {
+            _buttons[iButton].image.color = iButton == CurrButtonIndex ?
+                _selectedColor : _unselectedColor;
+        }
+    }
+
+    public void ResetScroll() {
+        _scrollRect.normalizedPosition = Vector2.up;
+        _currElementScrollIndex = 0;
+        PopulateButtons();
+    }
+}
