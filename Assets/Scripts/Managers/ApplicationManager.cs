84f100d404850581dcb1a83cb9cce4b840d2cc14
Martín Monterrosa <mmonterrosa94@gmail.com>
2021-10-04 22:13:41 -0300

    Add loading screen

--- Assets/Scripts/Managers/ApplicationManager.cs
+++ Assets/Scripts/Managers/ApplicationManager.cs
@@ -1,18 +1,28 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using Cysharp.Threading.Tasks;
 
 public class ApplicationManager : MonoBehaviourSingleton<ApplicationManager> {
-    [SerializeField] private CentralDatabase _centralDB = default;
+    public class Handle {
+        public bool IsComplete { get; private set; }
+        public void Complete() => IsComplete = true;
+    }
+
+    [SerializeField] private GameObject _loadingScreen;
+    private List<Handle> _loadingScreenHandles = new List<Handle>();
+    private CentralDatabase _centralDB;
     
     private async void Start() {
+        await Addressables.InitializeAsync();
+
+        _centralDB = await Addressables.LoadAssetAsync<CentralDatabase>(CentralDatabase.CentralDBAssetName);
+
         if (_centralDB == null) {
             UnityUtils.Quit();
             return;
         }
-
-        await Addressables.InitializeAsync();
 
         var databaseViewPopup = await PopupManager.Instance.GetOrLoadPopup<DatabaseViewPopup>();
         databaseViewPopup.Populate(_centralDB.DigimonDB);
@@ -59,5 +69,23 @@ public class ApplicationManager : MonoBehaviourSingleton<ApplicationManager> {
         if (Input.GetKeyDown(KeyCode.Escape)) {
             PopupManager.Instance.Back();
         }
+    }
+
+    public Handle DisplayLoadingScreen() {
+        Handle handle = new Handle();
+        _loadingScreenHandles.Add(handle);
+
+        if (_loadingScreenHandles.Count == 1) {
+            HideLoadingScreenOnceFinished();
+        }
+
+        return handle;
+    }
+
+    public async void HideLoadingScreenOnceFinished() {
+        _loadingScreen.SetActive(true);
+        await UniTask.WaitUntil(() => _loadingScreenHandles.TrueForAll(h => h.IsComplete));
+        _loadingScreen.SetActive(false);
+        _loadingScreenHandles.Clear();
     }
 }
