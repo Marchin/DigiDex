e7126f10a4505c3c973b7db7cd141976b060c894
Martín Monterrosa <mmonterrosa94@gmail.com>
2021-12-28 12:37:43 -0300

    Hide the FPS counter in release builds

--- Assets/Scripts/Utils/UI/FrameRateDisplay.cs
+++ Assets/Scripts/Utils/UI/FrameRateDisplay.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 public class FrameRateDisplay : MonoBehaviour {
     [SerializeField] private TextMeshProUGUI _text = default;
 
+    private void Awake() {
+#if !DEVELOPMENT_BUILD && !UNITY_EDITOR
+        gameObject.SetActive(false);
+#endif
+    }
+
     private void Update() {
         _text.text = Mathf.RoundToInt(1f / Time.deltaTime).ToString();
     }
